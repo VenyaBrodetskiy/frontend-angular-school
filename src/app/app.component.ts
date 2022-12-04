3bf3a77d4ac817d29590f459a12b13a1f95cee9f
Venya Brodetskiy <benjamincheg@gmail.com>
2022-12-04 15:02:47 +0200

    created component person-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
 
+interface IPerson {
+    name: string;
+    id: string;
+    address: string;
+    email: string;
+}
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
@@ -7,4 +13,24 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
     title = "frontend-nodejs-school";
+
+    public myProperty: string = "Hooray!";
+    public htmlProperty: string = "<i>La-la!!</i>"
+
+    public person: IPerson = {
+        name: "Venya",
+        id: "1",
+        address: "HaNeemanim",
+        email: "my@email.com"
+    }
+
+    public cardMessage: string = "";
+
+    public onClickMeClick(): void {
+        this.myProperty = 'Other data';
+    }
+
+    public onCardModeChanged(isEdit: boolean) {
+        this.cardMessage = isEdit ? "Please, fill the data" : "Data saved";
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
     declarations: [
@@ -10,10 +11,15 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        CoreModule
+    ],
+    exports: [
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
-    
+
 export class AppModule { };
